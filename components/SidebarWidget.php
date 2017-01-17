b64c972faba6ed8a858b4683d9401ecefa029866
Oleksander <khupchenko86@gmail.com>
2017-01-18 00:24:22 +0200

    формирование бокового меню неограниченной вложенности с таблицы categories

--- components/SidebarWidget.php
+++ components/SidebarWidget.php
@@ -2,6 +2,8 @@
 
 namespace app\components;
 use yii\base\Widget;
+use app\models\Category;
+use Yii;
 
 /**
  * Description of SidebarWidget
@@ -11,6 +13,9 @@ use yii\base\Widget;
 class SidebarWidget extends Widget {
     
     public $tpl;
+    public $data;
+    public $tree;
+    public $menuHtml;
     
     public function init() {
         parent::init();
@@ -21,6 +26,35 @@ class SidebarWidget extends Widget {
     }
     
     public function run() {
+        $this->data = Category::find()->indexBy('id')->asArray()->all();
+        $this->tree = $this->getTree();
+        $this->menuHtml = $this->getMenuHtml($this->tree);
+        return $this->menuHtml;
+    }
+    
+    protected function getTree() {
+        $tree = [];
+        foreach ($this->data as $id=>&$node) {
+            if(!$node['parent_id'])
+                $tree[$id] = &$node; 
+            else 
+                $this->data[$node['parent_id']]['childs'][$node['id']] = &$node;            
+        }
+        return $tree;
+    }
+    
+    protected function getMenuHtml($tree) {
+        $str = '';
+        foreach ($tree as $category) {
+            $str .= $this->catToTemplate($category);
+        }
+        return $str;
+    }
+    
+    protected function catToTemplate($category) {
+        ob_start();
         include __DIR__ . '/sidebar_tpl/' . $this->tpl;
+        $a = ob_get_clean();
+        return $a;
     }
 }
--- components/sidebar_tpl/menu.php
+++ components/sidebar_tpl/menu.php
@@ -1,15 +1,11 @@
-<ul class="nav nav-stacked">
-  <li role="presentation" class="active"><a href="#">Home</a></li>
-  <li role="presentation"><a href="#">Profile</a></li>
-  <li role="presentation"><a href="#">Messages</a></li>
-  <li role="presentation" class="active"><a href="#">Home</a></li>
-  <li role="presentation"><a href="#">Profile</a></li>
-  <li role="presentation"><a href="#">Messages</a></li>
-  <li role="presentation" class="active"><a href="#">Home</a></li>
-  <li role="presentation"><a href="#">Profile</a></li>
-  <li role="presentation"><a href="#">Messages</a></li>
-  <li role="presentation" class="active"><a href="#">Home</a></li>
-  <li role="presentation"><a href="#">Profile</a></li>
-  <li role="presentation"><a href="#">Messages</a></li>
-</ul>
+<li>
+    <a href="<?= \yii\helpers\Url::to(['category/index', 'id' => $category['id']])?>">
+        <?=$category['name']?>
+            <?php if(isset($category['childs']) ): ?>
+                <ul>
+                <?= $this->getMenuHtml($category['childs'])?>
+                </ul>    
+            <?php endif; ?>            
+    </a>    
+</li>
 
